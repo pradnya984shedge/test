9c25c5d838717212e7a7082a3dec0fcafdf237f6
prafull shedge <prafullshedge@gmail.com>
2020-04-26 12:00:39 +0100

    first commit

---
+++ exampleGrid/Models/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExampleGrid.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+        public DbSet<CustomerTB> CustomerTB { get; set; }
+    }
+}
